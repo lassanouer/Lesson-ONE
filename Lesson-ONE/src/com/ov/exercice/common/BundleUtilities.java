0fbab6393503fc074618c35dddb52b464f226f0a
Lassoued <lassanouer@users.noreply.github.com>
2016-09-25 17:56:30 +0200

    Update BundleUtilities.java

--- Lesson-ONE/src/com/ov/exercice/common/BundleUtilities.java
+++ Lesson-ONE/src/com/ov/exercice/common/BundleUtilities.java
@@ -1,17 +1,11 @@
-/*
-  BundleUtilities.java	java 1.7
-  
-  Created on 5 août 2015
-  Created By Mouna Boukari 
-  Copyright(c) 2015 Tunisie Télécom, Inc. All rights reserved.
-  This software is the proprietary information of Tunisie Télécom.Use is subject to license terms.    
-*/
 package com.ov.exercice.common;
 
 import java.util.ResourceBundle;
 
 /**
- * The Class BundleUtilities.
+ * 
+ * @author Anouer Lassoued
+ *
  */
 public class BundleUtilities {
 
